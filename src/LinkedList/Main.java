0917eb1daa104bda4ff44dc2d385c6fbfc5f817c
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-02-09 17:55:07 +0530

    ll

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -3,5 +3,15 @@ package LinkedList;
 public class Main {
     public static void main(String[] args) {
         linkedlist list = new linkedlist();
+
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(4);
+        list.insertFirst(17);
+        list.insertLast(99);
+
+        list.insert(100,3);
+
+        list.display();
     }
 }
